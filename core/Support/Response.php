8c9d1ed970cb9b075f3f96b939c0241f9a129c38
Dung Nguyen Dang <dangdungcntt@gmail.com>
2020-07-09 14:34:45 +0700

    Cleanup Response class

--- core/Support/Response.php
+++ core/Support/Response.php
@@ -4,16 +4,10 @@ namespace Core\Support;
 
 use React\Http\Io\HttpBodyStream;
 use React\Stream\ReadableStreamInterface;
+use function RingCentral\Psr7\stream_for;
 
 class Response extends \React\Http\Response
 {
-    public function withHeaders(array $headers)
-    {
-        $new = clone $this;
-        $new->setHeaders($headers);
-        return $new;
-    }
-
     public function stream(ReadableStreamInterface $stream): Response
     {
         return $this->withBody(new HttpBodyStream($stream, null));
@@ -21,17 +15,21 @@ class Response extends \React\Http\Response
 
     public function json($data, int $status = 200): Response
     {
-        $headers                 = $this->getHeaders();
-        $headers['Content-Type'] = 'application/json';
-
-        return new Response($status, $headers, json_encode($data));
+        return $this
+            ->withHeader('Content-Type', 'application/json')
+            ->withBody(stream_for(json_encode($data)))
+            ->withStatus($status);
     }
 
     public function redirect(string $to, int $status = 302, array $headers = []): Response
     {
-        $headers             = array_merge($this->getHeaders(), $headers);
-        $headers['Location'] = $to;
+        return $this->withHeader('Location', $to)->withHeaders($headers);
+    }
 
-        return new Response($status, $headers);
+    public function withHeaders(array $headers)
+    {
+        $new = clone $this;
+        $new->setHeaders($headers);
+        return $new;
     }
 }
