3123178b1c9814307b51c508ee94167f80f27bac
Dung Nguyen Dang <dangdungcntt@gmail.com>
2020-12-10 13:02:28 +0700

    Upgrade framework to 1.1.0

--- bootstrap.php
+++ bootstrap.php
@@ -1,5 +1,8 @@
 <?php
 
+//From v1.1.0 using symfony/var-dumper
+$GLOBALS['__composer_autoload_files'][md5('symfony/var-dumper:Resources/functions/dump.php')] = true;
+
 require_once __DIR__.'/vendor/autoload.php';
 
 define('__ROOT__', __DIR__);
